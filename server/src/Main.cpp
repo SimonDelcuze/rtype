5892517157c81f248aeeae78da839a96f4d43e67
louistruptil <louis.truptil@epitech.eu>
2025-11-20 16:52:26 +0100

    fix: format code

--- server/src/Main.cpp
+++ server/src/Main.cpp
@@ -1,4 +1,5 @@
 #include "Main.hpp"
+
 #include "Logger.hpp"
 
 #include <string>
@@ -7,19 +8,16 @@ int main(int argc, char* argv[])
 {
     bool verbose = false;
 
-    for (int i = 1; i < argc; ++i)
-    {
+    for (int i = 1; i < argc; ++i) {
         const std::string argument(argv[i]);
-        if (argument == "--verbose" || argument == "-v")
-        {
+        if (argument == "--verbose" || argument == "-v") {
             verbose = true;
         }
     }
 
     Logger::instance().setVerbose(verbose);
 
-    if (verbose)
-    {
+    if (verbose) {
         Logger::instance().info("Verbose logging enabled");
     }
 
