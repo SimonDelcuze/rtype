a5db02276083f34efb551fc24d18e49e3a9a74d8
Simon Delcuze <simon.delcuze@epitech.eu>
2025-11-22 18:21:08 +0100

    feat(ecs/shared): interfaces for scheduler and systems

--- client/include/systems/AnimationSystem.hpp
+++ client/include/systems/AnimationSystem.hpp
@@ -3,11 +3,12 @@
 #include "components/AnimationComponent.hpp"
 #include "components/SpriteComponent.hpp"
 #include "ecs/Registry.hpp"
+#include "systems/ISystem.hpp"
 
-class AnimationSystem
+class AnimationSystem : public ISystem
 {
   public:
-    void update(Registry& registry, float deltaTime);
+    void update(Registry& registry, float deltaTime) override;
 
   private:
     void advanceFrame(AnimationComponent& anim);
