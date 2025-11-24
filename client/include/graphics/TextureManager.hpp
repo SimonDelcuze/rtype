c0c87d892baba18bcceb78b22f5d278dccdc1154
Charliinew <charles.ignoux@epitech.eu>
2025-11-24 16:30:09 +0100

    feat: utils fonctions for texture manager and some tests

--- client/include/graphics/TextureManager.hpp
+++ client/include/graphics/TextureManager.hpp
@@ -9,7 +9,10 @@ class TextureManager
   public:
     const sf::Texture& load(const std::string& id, const std::string& path);
     const sf::Texture* get(const std::string& id) const;
+    bool has(const std::string& id) const;
+    void remove(const std::string& id);
     void clear();
+    std::size_t size() const;
 
   private:
     std::unordered_map<std::string, sf::Texture> textures_;
