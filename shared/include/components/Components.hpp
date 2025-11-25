12bf89ca1951e34a89a66056e0f846b42c9a6a41
louistruptil <louis.truptil@epitech.eu>
2025-11-25 11:30:08 +0100

    fix: format code

--- shared/include/components/Components.hpp
+++ shared/include/components/Components.hpp
@@ -3,7 +3,6 @@
 #include "components/HealthComponent.hpp"
 #include "components/HitboxComponent.hpp"
 #include "components/LayerComponent.hpp"
-#include "components/PlayerInputComponent.hpp"
 #include "components/MovementComponent.hpp"
 #include "components/OwnershipComponent.hpp"
 #include "components/PlayerInputComponent.hpp"
