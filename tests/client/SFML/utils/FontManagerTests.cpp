e0529385eddb0ed61e8fff498e28ddcd5e7fefa8
Charliinew <charles.ignoux@epitech.eu>
2025-11-26 20:25:51 +0100

    test: font manager and sounds

---
+++ tests/client/SFML/utils/FontManagerTests.cpp
@@ -0,0 +1,120 @@
+#include "TestAssets.hpp"
+#include "graphics/FontManager.hpp"
+
+#include <gtest/gtest.h>
+
+TEST(FontManager, LoadGetAndClear)
+{
+    FontManager manager;
+    const auto& font = manager.load("test_font", assetPath("fonts/test.ttf"));
+
+    EXPECT_NE(&font, nullptr);
+
+    const sf::Font* fetched = manager.get("test_font");
+    EXPECT_NE(fetched, nullptr);
+    EXPECT_EQ(fetched, &font);
+
+    manager.clear();
+    EXPECT_EQ(manager.get("test_font"), nullptr);
+}
+
+TEST(FontManager, LoadTwiceReplacesExisting)
+{
+    FontManager manager;
+    const auto& first  = manager.load("test_font", assetPath("fonts/test.ttf"));
+    const auto& second = manager.load("test_font", assetPath("fonts/test.ttf"));
+
+    EXPECT_NE(&first, nullptr);
+    EXPECT_NE(&second, nullptr);
+    EXPECT_EQ(manager.get("test_font"), &second);
+}
+
+TEST(FontManager, LoadThrowsOnMissingFile)
+{
+    FontManager manager;
+    EXPECT_THROW(manager.load("missing", assetPath("fonts/does_not_exist.ttf")), std::runtime_error);
+}
+
+TEST(FontManager, GetUnknownReturnsNullptr)
+{
+    FontManager manager;
+    EXPECT_EQ(manager.get("unknown"), nullptr);
+}
+
+TEST(FontManager, FailedLoadDoesNotInsert)
+{
+    FontManager manager;
+    EXPECT_THROW(manager.load("bad", assetPath("fonts/nope.ttf")), std::runtime_error);
+    EXPECT_EQ(manager.get("bad"), nullptr);
+}
+
+TEST(FontManager, HasReturnsTrueForLoaded)
+{
+    FontManager manager;
+    EXPECT_FALSE(manager.has("test_font"));
+    manager.load("test_font", assetPath("fonts/test.ttf"));
+    EXPECT_TRUE(manager.has("test_font"));
+}
+
+TEST(FontManager, RemoveDeletesFont)
+{
+    FontManager manager;
+    manager.load("test_font", assetPath("fonts/test.ttf"));
+    EXPECT_TRUE(manager.has("test_font"));
+
+    manager.remove("test_font");
+    EXPECT_FALSE(manager.has("test_font"));
+    EXPECT_EQ(manager.get("test_font"), nullptr);
+}
+
+TEST(FontManager, RemoveNonexistentDoesNotCrash)
+{
+    FontManager manager;
+    EXPECT_NO_THROW(manager.remove("nonexistent"));
+}
+
+TEST(FontManager, SizeReturnsCorrectCount)
+{
+    FontManager manager;
+    EXPECT_EQ(manager.size(), 0u);
+
+    manager.load("font1", assetPath("fonts/test.ttf"));
+    EXPECT_EQ(manager.size(), 1u);
+
+    manager.load("font2", assetPath("fonts/test.ttf"));
+    EXPECT_EQ(manager.size(), 2u);
+
+    manager.remove("font1");
+    EXPECT_EQ(manager.size(), 1u);
+
+    manager.clear();
+    EXPECT_EQ(manager.size(), 0u);
+}
+
+TEST(FontManager, ReloadPreservesId)
+{
+    FontManager manager;
+    manager.load("test_font", assetPath("fonts/test.ttf"));
+    const sf::Font* first = manager.get("test_font");
+
+    manager.load("test_font", assetPath("fonts/test.ttf"));
+    const sf::Font* second = manager.get("test_font");
+
+    EXPECT_NE(first, nullptr);
+    EXPECT_NE(second, nullptr);
+    EXPECT_EQ(manager.size(), 1u);
+}
+
+TEST(FontManager, MultipleFontsIndependent)
+{
+    FontManager manager;
+    manager.load("font1", assetPath("fonts/test.ttf"));
+    manager.load("font2", assetPath("fonts/test.ttf"));
+
+    EXPECT_NE(manager.get("font1"), manager.get("font2"));
+    EXPECT_EQ(manager.size(), 2u);
+
+    manager.remove("font1");
+    EXPECT_FALSE(manager.has("font1"));
+    EXPECT_TRUE(manager.has("font2"));
+}
