838a969340afa75b1bfd17df6e1d7d14810b5ac2
Ezio Decadt <eziodecadt04@gmail.com>
2025-12-15 16:47:34 +0100

    style: format code

--- server/src/systems/BoundarySystem.cpp
+++ server/src/systems/BoundarySystem.cpp
@@ -7,8 +7,8 @@ void BoundarySystem::update(Registry& registry) const
     for (EntityId id : registry.view<TransformComponent, BoundaryComponent>()) {
         if (!registry.isAlive(id))
             continue;
-        auto& transform      = registry.get<TransformComponent>(id);
-        const auto& bounds   = registry.get<BoundaryComponent>(id);
+        auto& transform    = registry.get<TransformComponent>(id);
+        const auto& bounds = registry.get<BoundaryComponent>(id);
 
         transform.x = std::clamp(transform.x, bounds.minX, bounds.maxX);
         transform.y = std::clamp(transform.y, bounds.minY, bounds.maxY);
