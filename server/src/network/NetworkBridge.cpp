1c10390342c7640ef217ca949b90bd8ee2866531
Ezio Decadt <eziodecadt04@gmail.com>
2026-01-06 20:13:14 +0100

    feat(server): introduce NetworkBridge to decouple simulation from network

---
+++ server/src/network/NetworkBridge.cpp
@@ -0,0 +1,40 @@
+#include "network/NetworkBridge.hpp"
+
+#include "network/EntityDestroyedPacket.hpp"
+#include "network/EntitySpawnPacket.hpp"
+
+NetworkBridge::NetworkBridge(SendThread& sendThread) : sendThread_(sendThread)
+{
+}
+
+void NetworkBridge::processEvents(const std::vector<GameEvent>& events)
+{
+    for (const auto& event : events) {
+        std::visit(
+            [this](auto&& evt) {
+                using T = std::decay_t<decltype(evt)>;
+                if constexpr (std::is_same_v<T, EntitySpawnedEvent>) {
+                    EntitySpawnPacket pkt{};
+                    pkt.entityId   = evt.entityId;
+                    pkt.entityType = evt.entityType;
+                    pkt.posX       = evt.posX;
+                    pkt.posY       = evt.posY;
+                    sendThread_.broadcast(pkt);
+                    knownEntities_.insert(evt.entityId);
+                } else if constexpr (std::is_same_v<T, EntityDestroyedEvent>) {
+                    EntityDestroyedPacket pkt{};
+                    pkt.entityId = evt.entityId;
+                    sendThread_.broadcast(pkt);
+                    knownEntities_.erase(evt.entityId);
+                } else if constexpr (std::is_same_v<T, CollisionEvent>) {
+                    // Could be used for sound/FX in the future
+                }
+            },
+            event);
+    }
+}
+
+void NetworkBridge::clear()
+{
+    knownEntities_.clear();
+}
