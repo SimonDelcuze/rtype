ba52a83c4fe9fc5d151852568f0abb5bc22ddebd
Charliinew <charles.ignoux@epitech.eu>
2026-01-07 16:15:54 +0100

    chore: format code

--- tests/server/systems/MissileIntegrationTests.cpp
+++ tests/server/systems/MissileIntegrationTests.cpp
@@ -1,9 +1,9 @@
 #define _USE_MATH_DEFINES
-#include <cmath>
 #include "simulation/PlayerCommand.hpp"
 #include "systems/MovementSystem.hpp"
 #include "systems/PlayerInputSystem.hpp"
 
+#include <cmath>
 #include <gtest/gtest.h>
 
 static PlayerCommand inputWithFlags(std::uint32_t playerId, std::uint16_t seq, std::uint16_t flags, float x, float y,
