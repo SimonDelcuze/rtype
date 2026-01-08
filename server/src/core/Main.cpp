3c5c2fb6af07eedd2302882103b2691ac7fc3893
Simon Delcuze <simon.delcuze@epitech.eu>
2026-01-09 00:00:41 +0100

    chore: format code

--- server/src/core/Main.cpp
+++ server/src/core/Main.cpp
@@ -6,8 +6,8 @@
 
 int main(int argc, char* argv[])
 {
-    (void)argc;
-    (void)argv;
+    (void) argc;
+    (void) argv;
     Logger::instance().setVerbose(true);
     Logger::instance().loadTagConfig("server.log.config");
     run_server();
