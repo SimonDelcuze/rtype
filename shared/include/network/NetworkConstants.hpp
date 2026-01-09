dc6615c6e226ee2dd121687f3707a01d2c2fc694
Simon Delcuze <simon.delcuze@epitech.eu>
2026-01-09 20:23:11 +0100

    chore: format code

--- shared/include/network/NetworkConstants.hpp
+++ shared/include/network/NetworkConstants.hpp
@@ -6,10 +6,10 @@
 
 namespace Network
 {
-    constexpr std::size_t kEthernetMTU = 1500;
-    constexpr std::size_t kIPv4HeaderSize = 20;
-    constexpr std::size_t kUDPHeaderSize = 8;
-    constexpr std::size_t kMaxUDPPayload = kEthernetMTU - kIPv4HeaderSize - kUDPHeaderSize;
+    constexpr std::size_t kEthernetMTU          = 1500;
+    constexpr std::size_t kIPv4HeaderSize       = 20;
+    constexpr std::size_t kUDPHeaderSize        = 8;
+    constexpr std::size_t kMaxUDPPayload        = kEthernetMTU - kIPv4HeaderSize - kUDPHeaderSize;
     constexpr std::size_t kMaxSafePacketPayload = kMaxUDPPayload - 20 - PacketHeader::kSize - PacketHeader::kCrcSize;
 
 } // namespace Network
